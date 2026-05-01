63bac8b829750a6dce24a2f8395e3fa506ebc9fc
Manthan Martiwar <manthanmartiwar61@gmail.com>
2026-05-01 14:10:45 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class main{
+public static void main(String[] args) {
+
+System.out.println("this is java program done in vs code");
+System.out.println("outpur from java program");
+System.out.println(new Date());
+
+
+
+}
+}
